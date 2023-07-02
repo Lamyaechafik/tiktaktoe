f11fe469e2b476dde41ec29220260e8e54ed1c1e
Lamyae Chafik <lamyaechafik@Lamyaes-MBP.lan>
2023-07-02 02:35:39 -0400

    updated

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
 let title = document.querySelector('.title');
 let turn = 'x';
 let squares = [];
-function end(num1,num2,num3)
+function end(num1, num2, num3)
 {
-    title.innerHTML = `${squares[1]} winner`;
+    title.innerHTML = `${squares[num1]} winner`;
         document.getElementById('item'+num1).style.background = '#000';
         document.getElementById('item'+num2).style.background = '#000';
         document.getElementById('item'+num3).style.background = '#000';
         setInterval(function(){title.innerHTML += '.'},1000);
-        setTimeout(function(){location.reload},400);
+        setTimeout(function(){location.reload()},4000);
 }
 function winner () 
 {
     for(let i = 1; i<10 ;i++)
     {
-        squares[i] = document.getElementById('item' +i);
+        squares[i] = document.getElementById('item' +i).innerHTML;
     }
-    if(squares[1] == squares[2] && squares[2] == squares == [3] && squares[1] != '')
+    if(squares[1] == squares[2] && squares[2] == squares [3] && squares[1] != '')
     {
-        end(1,2,3);
+        end(1, 2, 3);
     }
     else if(squares[4] == squares[5] && squares[5] == squares[6] && squares[5] != '')
     {
-        end(4,5,6);
+        end(4, 5, 6);
     } 
     else if(squares[7] == squares[8] && squares[8] == squares[9] && squares[9] != '')
     {
-        end(7,8,9);
+        end(7, 8, 9);
     }
 
 
     else if(squares[1] == squares[4] && squares[4] == squares[7] && squares[7] != '')
     {
-        end(1,4,7);
+        end(1, 4, 7);
     }
     else if(squares[2] == squares[5] && squares[5] == squares[8] && squares[8] != '')
     {
-        end(2,5,8);
+        end(2, 5, 8);
     }
     else if(squares[3] == squares[6] && squares[6] == squares[9] && squares[9] != '')
     {
-        end(3,6,9);
+        end(3, 6, 9);
     }
 
 
     else if(squares[1] == squares[5] && squares[5] == squares[9] && squares[9] != '')
     {
-        end(1,5,9);
+        end(1, 5, 9);
     }
     else if(squares[3] == squares[5] && squares[5] == squares[7] && squares[7] != '')
     {
-        end(3,5,7);
+        end(3, 5, 7);
     }
 
 
@@ -73,9 +73,9 @@ function game(id)
         turn = 'x';
         title.innerHTML = 'x';
     }
+    winner();
 
 
     }
-
 
 
